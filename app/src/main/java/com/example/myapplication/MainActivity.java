e7ac803da4a4ae6db6cff866daf5c515f8334410
Dima Vlad <dimavlad31@yahoo.com>
2025-02-25 11:41:46 +0200

    Laboratorul 2 Extra

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -29,5 +30,27 @@ public class MainActivity extends AppCompatActivity {
     public void butonPushed2(View view) {
         TextView textView = findViewById(R.id.textView);
         textView.setText(getString(R.string.Button_Pushed_string));
+        TextView textView2 = findViewById(R.id.textView2);
+        EditText firstNumber = findViewById(R.id.editTextText2);
+        EditText SecondNumber = findViewById(R.id.editTextText3);
+        String firstInput = firstNumber.getText().toString();
+        String secondInput = SecondNumber.getText().toString();
+
+
+        if (!firstInput.isEmpty() && !secondInput.isEmpty()) {
+            try {
+
+                int num1 = Integer.parseInt(firstInput);
+                int num2 = Integer.parseInt(secondInput);
+                int sum = num1 + num2;
+
+                textView2.setText("Rezultat: " + sum);
+            } catch (NumberFormatException e) {
+                textView2.setText("Introduceți doar numere valide!");
+            }
+        } else {
+            textView2.setText("Completați ambele câmpuri!");
+        }
+
     }
 }
